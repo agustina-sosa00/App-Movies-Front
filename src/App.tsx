c456991728f600888081bf59cdb013061b331f64
leandro mortarini <leandromortarini2@gmail.com>
2025-10-13 10:06:48 -0300

    create button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import Button from "./components/Button";
+// import { IoMdClose } from "react-icons/io";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-red-500">Movies App</h1>
+    <div className="flex justify-center items-center h-screen">
+      {/* <h1 className="text-red-500">Movies App</h1> */}
+      <Button
+        text="Button"
+        type="primary"
+        // height="h-[200px]"
+        // width="w-[200px]"
+        // icon={IoMdClose}
+        // positionIcon="right"
+      />
     </div>
   );
 }
